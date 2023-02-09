463f91dbbba2db55fa559b0343d38308f3bb6bf9
henrilja <henrilja@stud.ntnu.no>
2023-02-09 22:15:44 +0100

    Part 8, No CSS changes

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Home</h2>
+        </div>
+    )
+}
+
+export default Home
