38e14b036e451fe7851293e3038647a4c1a9a685
anerak <anerak@stud.ntnu.no>
2023-02-17 11:37:52 +0100

    #26 Prøver å lage to tester, men begge feiler

--- frontend/src/components/ExerciseForm.test.js
+++ frontend/src/components/ExerciseForm.test.js
@@ -1,26 +1,32 @@
 import renderer from 'react-test-renderer';
-import WorkoutForm from '../WorkoutForm';
+import ExerciseForm from './ExerciseForm';
 
 it('changes the class when hovered', () => {
   const component = renderer.create(
-    <Link page="http://www.facebook.com">Facebook</Link>,
+    <ExerciseForm page="http://www.facebook.com">Facebook</ExerciseForm>,
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
   // manually trigger the callback
   renderer.act(() => {
-    tree.props.onMouseEnter();
+    tree.props().handleSubmit;
   });
+
+})
+  /*
   // re-rendering
   tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
+})
+  /*
   // manually trigger the callback
   renderer.act(() => {
-    tree.props.onMouseLeave();
+    tree.onChange();
   });
   // re-rendering
   tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+*/
