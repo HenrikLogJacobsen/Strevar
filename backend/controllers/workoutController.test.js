2a784058ad1705d9b10b8daf7f01f73e6b802052
anerak <anerak@stud.ntnu.no>
2023-02-22 11:49:48 +0100

    #21 Ikke mange endringer da testingen er vanskelig

--- backend/controllers/workoutController.test.js
+++ backend/controllers/workoutController.test.js
@@ -1,30 +1,15 @@
-const workoutController = require('./workoutController')
+const workoutController = require('./workoutController');
+//const validateID = workoutController.validateID({id:'63f5cb3801753708f42761a5'},{message:'Dette er en test'},{res:''});
 const getWorkout = workoutController.getWorkout;
 const getAllWorkouts = workoutController.getAllWorkouts;
 const createWorkout = workoutController.createWorkout;
-const { mockRequest, mockResponse } = require('./util/interceptor')
+const { mockRequest, mockResponse } = require('../util/interceptor')
 
 
-
-//jest.setTimeout(60000);
-
-describe("Check function \'getWorkout\' ", () => {
-  test('should 200 and return correct value', async () => {
-    let req = mockRequest();
-    req.body = 'hei';
-    const res = mockResponse();
-
-    await createWorkout(req, res);
-
-    expect(res.status).toHaveBeenCalledTimes(1)
-    expect(res.status.mock.calls.length).toBe(1);
-    expect(res.status(200).json).toHaveBeenCalledWith('yo');
-  });
-})
+jest.setTimeout(60000);
 
 
-
-describe("Check function \'getWorkout\' ", () => {
+describe("Check function \'createWorkout\' ", () => {
   test('should 200 and return correct value', async () => {
     let req = mockRequest();
     req.params.id = '61';
@@ -37,6 +22,84 @@ describe("Check function \'getWorkout\' ", () => {
     expect(res.status(404).json).toHaveBeenCalledWith({error: "ID spesifisert er ikke gyldig"});
   });
 })
+
+
+/*
+describe("Check function \'getAllWorkouts\' ", () => {
+  test('should 200 and return correct value', async () => {
+    let req = mockRequest();
+    //req.params.id = '61';
+    const res = mockResponse();
+
+    await getAllWorkouts(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.status.mock.calls.length).toBe(1);
+    expect(res.status(404).json).toHaveBeenCalledWith({error: "ID spesifisert er ikke gyldig"});
+  });
+})
+*/
+
+describe("Check function \'createWorkout\' ", () => {
+  test('TEST 1, should 200 and return correct value', async () => {
+    
+    let req = mockRequest();
+    req.body.tittel ='benk';
+    req.body.sett = 12;
+    req.body.repetisjoner = 13; 
+    req.body.vekt = 100;
+    const res = mockResponse();
+    await createWorkout(req, res);
+
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status.mock.calls.length).toBe(1);
+    expect(req.body.tittel).toBe('benk');
+    expect(req.body.sett).toBe(12);
+    expect(req.body.repetisjoner).toBe(13);
+    expect(req.body.vekt).toBe(100);
+    //expect(res.status(200).json).toHaveBeenCalledWith('yo');
+  });
+})
+
+
+/*
+describe("Check function \'validateID\' ", () => {
+  test('should 200 and return correct value', () => {
+    let id = '63f5cb3801753708f42761a5';
+    let message = 'Dette er en test'
+    const res = mockResponse();
+
+    validateID(id,message,res);
+
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.status.mock.calls.length).toBe(1);
+    expect(res.status(404).json).toHaveBeenCalledWith({error: "ID spesifisert er ikke gyldig"});
+  });
+})
+
+*/
+/*
+
+describe("Check function \'getWorkout\' ", () => {
+  test('TEST 2, should 200 and return correct value', async () => {
+    let req = mockRequest();
+    req.params.id = '63de85f61be0fe13117db866';
+    const res = mockResponse();
+
+    await getWorkout(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.status.mock.calls.length).toBe(1);
+    expect(res.status(200).json(res)).toHaveBeenCalledWith({error: "ID spesifisert er ikke gyldig"});
+  });
+})
+*/
+
+
+
+
+
 
 
   //Tror vi må ha verdier i databasen for at denne skal kjøre
@@ -71,21 +134,7 @@ describe("Check function \'getAllWorkouts\' ", () => {
 
 */
 
-/*
-describe("Check function \'createWorkout\' ", () => {
-  test('should 200 and return correct value', async () => {
-    let req = mockRequest();
-    req.params.id = '61';
-    const res = mockResponse();
 
-    await createWorkout(req, res);
-
-    expect(res.status).toHaveBeenCalledTimes(1)
-    expect(res.status.mock.calls.length).toBe(1);
-    expect(res.status(404).json).toHaveBeenCalledWith({error: "Ikke gyldig treningsoktID"});
-  });
-})
-*/
   //
   /*
 
