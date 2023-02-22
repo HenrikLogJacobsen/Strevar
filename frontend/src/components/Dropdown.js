14f62bb6cc952cd3cc0d2f6bfe77639d70ee8470
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-02-22 13:16:52 +0100

    #8 dropdown med avkrysning

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -9,10 +9,10 @@ const Icon = () => {
   }
   
   
-const Dropdown = ({ placeHolder, options }) => {
-
+const Dropdown = ({ placeHolder, options, isMulti }) => {
+    
     const [showMenu, setShowMenu] = useState(false)
-    const [selectedValue, setSelectedValue] = useState(null)
+    const [selectedValue, setSelectedValue] = useState(isMulti ? [] : null)
 
     useEffect(() => {
         const handler = () => setShowMenu(false)
@@ -28,18 +28,67 @@ const Dropdown = ({ placeHolder, options }) => {
         setShowMenu(!showMenu)
     }
 
+    const CloseIcon = () => {
+        return (
+            <svg height="20" width="20" viewBox="0 0 20 20">
+                <path d="M14.348 14.849c-0.469 0.469-1.229 
+                0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 
+                0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 
+                0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 
+                0-1.697s1.228-0.469 1.697 0l2.652 3.031 
+                2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 
+                1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path>
+            </svg>
+        )
+    }
+
     const getDisplay = () => {
-        if (selectedValue) {
-            return selectedValue.label
+        if (!selectedValue || selectedValue.length === 0) {
+            return placeHolder
         }
-        return placeHolder;
+        if (isMulti) {
+            return (
+                <div className="dropdown-tags">
+                    {selectedValue.map((option) => (
+                        <div key={option.value} className="dropdown-tag-item">
+                            {option.label}
+                            <span onClick={(e) => onTagRemove(e, option)} className="dropdown-tag-close"><CloseIcon /></span>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
+        return selectedValue.label
+    }
+
+    const removeOption = (option) => {
+        return selectedValue.filter((o) => o.value !== option.value)
+    }
+
+    const onTagRemove = (e, option) => {
+        e.stopPropagation()
+        setSelectedValue(removeOption(option))
     }
 
     const onItemClick = (option) => {
-        setSelectedValue(option)
+        let newValue;
+        if (isMulti) {
+            if (selectedValue.findIndex((o) => o.value === option.value) >= 0) {
+                newValue = removeOption(option)
+            } else {
+                newValue = [...selectedValue, option]
+            }
+        } else {
+            newValue = option
+        }
+        setSelectedValue(newValue)
     }
 
     const isSelected = (option) => {
+        if (isMulti) {
+            return selectedValue.filter((o) => o.value === option.value).length > 0
+        }
+
         if (!selectedValue) {
             return false
         }
