dea89b5aedb82642d26f91271fb3ed5dceebb4ad
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-03-02 15:51:52 +0100

    #10 oppdatere styling på dropdown-meny

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useSessionCtx } from "../hooks/useSessionCtx"
 
-const Icon = () => {
+const Icon = ({rotate}) => {
     return (
-      <svg height="20" width="20" viewBox="0 0 20 20">
+      <svg
+      style={rotate ? {} : {transform: 'rotate(270deg)'}}
+      height="20" width="20" viewBox="0 0 20 20">
         <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
       </svg>
     )
@@ -133,7 +135,7 @@ const Dropdown = ({ placeHolder, options, isMulti, allExercises }) => {
       <div className="dropdown-container">
         <input
             className='sessionInput'
-            placeholder="Navn på økt"
+            placeholder="Navn på økt..."
             type = "text"
             onChange = {(e) => setTitle(e.target.value)}
             value = {title}
@@ -142,7 +144,7 @@ const Dropdown = ({ placeHolder, options, isMulti, allExercises }) => {
           <div className="dropdown-selected-value">{getDisplay()}</div>
           <div className="dropdown-tools">
             <div className="dropdown-tool">
-              <Icon />
+              <Icon rotate={showMenu} />
             </div>
           </div>
         </div>
