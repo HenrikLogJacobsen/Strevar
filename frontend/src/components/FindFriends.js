4d18dc7b7f23ba60c51a874c20c46cbcdbcdd95b
anerak <anerak@stud.ntnu.no>
2023-03-14 19:23:24 +0100

    #48 Fikset funksjonalitet for følg-knapp
    Co-authored-by: Vebjørn Haugland <vebbis@users.noreply.github.com>

--- frontend/src/components/FindFriends.js
+++ frontend/src/components/FindFriends.js
@@ -23,7 +23,47 @@ const FindFriends = () => {
   }, [dispatchUser])
 
 
+//   const handleClick = async (e) => {
+//     e.preventDefault()
 
+//     // POST NEW SESSION
+//     if (selectedValue.length !== 0) {
+//         const exercises = selectedValue.map((selected) => {
+//             return allExercises.find((exercise) => selected.value === exercise._id)
+//         })
+//         const user_id = user.uid
+//         const session = {title, exercises, user_id}
+//         const response = await fetch("/api/sessions/", {
+//             method: "POST",
+//             body: JSON.stringify(session),
+//             headers: {
+//                 "Content-Type": "application/json"
+//             }
+//         })
+
+//         const json = await response.json()
+
+//         if(response.ok) {
+//             setTitle('')
+//             setSelectedValue([])
+//             console.log("Ny treningsøkt lagt til", json)
+//             dispatchSession({type: "CREATE_SESSION", payload: json})
+//         }
+//     }
+// }
+
+const hhandleClick = async (userId) => {
+    // Send a request to add the user's id to the current user's following array
+    console.log(userId)
+    // const response = await fetch(`/api/users/${userId}/follow`, {
+    //   method: 'PUT',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ followeeId: user._id }),
+    // });
+    // if (!response.ok) {
+    //   throw new Error('Unable to follow user');
+    // }
+  }
 
   return (
     <div className="findFriends">
@@ -31,7 +71,8 @@ const FindFriends = () => {
           <div className="friendElement" key={user._id}>
               <span className="friendInfo">{user.username}</span>
               <span className="followButton">
-                  <button className="button">Følg</button> 
+                  <button className="button" onClick={() => hhandleClick(user._id)}>Følg</button> 
+
               </span>
           </div>
       ))}
