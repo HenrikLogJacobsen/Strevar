de7d6d69de14e13b4a72b167ece69c56028155a9
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-03-15 11:52:34 +0100

    Co-authored-by: Martine Østvik <martostv@stud.ntnu.no>

--- frontend/src/components/CreateGroups/CreateGroups.js
+++ frontend/src/components/CreateGroups/CreateGroups.js
@@ -1,11 +1,44 @@
 import React from 'react'
+
 import './createGroups.css'
 
+import { useState } from "react"
+import { useUaCtx } from "../../hooks/useUaCtx"
+
+
 const CreateGroups = () => {
-  // const [title, setTitle] = useState('')
-  // const [workoutType, setWorkoutType] = useState('')
-  // const [description, setDescription] = useState('')
+
+  const [groupName, setGroupName] = useState('')
+  const [description, setDescription] = useState('')
+  const [administrator, setAdministrator] = useState('')
+  const [members, setMembers] = useState('')
+  const [image, setImage] = useState('')
+  const [error, setError] = useState(null)
+
+  const { user } = useUaCtx()
   
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const user_id = user.uid;
+    const group = {groupName, description, administrator, members, image}
+    const response = await fetch("/api/groups/", {
+        method: "POST",
+        body: JSON.stringify(group),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    const json = await response.json()
+
+    if(!response.ok) {
+        setError(json.error)
+    } 
+    
+}
+
 
   return (
     <form className="createGroups" /*onSubmit={handleSubmit}*/>
