f9bc8ae944b07875172c78fb263fbe3476c24b7f
martostv <martostv@stud.ntnu.no>
2023-03-15 11:16:55 +0100

    #38 endret groups

--- frontend/src/components/CreateGroups/CreateGroups.js
+++ frontend/src/components/CreateGroups/CreateGroups.js
@@ -2,15 +2,38 @@ import React from 'react'
 import './createGroups.css'
 
 const CreateGroups = () => {
-
+  // const [title, setTitle] = useState('')
+  // const [workoutType, setWorkoutType] = useState('')
+  // const [description, setDescription] = useState('')
+  
 
   return (
-    <form className="createExercise" onSubmit={handleSubmit}>
-            <h3>Legg til øvelse</h3>
+    <form className="createGroups" /*onSubmit={handleSubmit}*/>
+      <h3>Ny gruppe</h3>
       
-      <input>
-      
-      </input>
+      <input
+        placeholder="Gruppenavn"
+        type = "text"
+        // onChange = {(e) => setTitle(e.target.value)}
+        // value = {title}
+      />
+
+      <input 
+        placeholder="Treningstype"
+        type = "text"
+        // onChange = {(e) => setWorkoutType(e.target.value)}
+        // value = {workoutType}
+      />
+
+      <input 
+        placeholder="Beskrivelse"
+        type = "text"
+        // onChange = {(e) => setDescription(e.target.value)}
+        // value = {description}
+      />
+
+      <button className="button">Opprett gruppe</button>
+      {/* {error && <div className="error">{error}</div>} */}
     </form>
   )
 }
